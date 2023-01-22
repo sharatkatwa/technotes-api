6c41e8b78ecd889ec10eaed8df81cca46f73cd08
sharatkatwa <sjkatwa2000@gmail.com>
2023-01-22 18:31:06 +0530

    added user forms

--- tech-notes-react/src/App.js
+++ tech-notes-react/src/App.js
@@ -6,6 +6,10 @@ import DashLayout from './components/DashLayout';
 import Welcome from './features/auth/Welcome';
 import NotesList from './features/notes/NotesList';
 import UsersList from './features/users/UsersList';
+import NewUserForm from './features/users/NewUserForm';
+import EditUser from './features/users/EditUser';
+import NewNote from './features/notes/NewNote';
+import EditNote from './features/notes/EditNote';
 
 function App() {
   return (
@@ -16,11 +20,17 @@ function App() {
 
         <Route path='dash' element={<DashLayout />}>
           <Route index element={<Welcome />} />
-          <Route path='notes'>
-            <Route index element={<NotesList />} />
-          </Route>
+
           <Route path='users'>
             <Route index element={<UsersList />} />
+            <Route path=':id' element={<EditUser />} />
+            <Route path='new' element={<NewUserForm />} />
+          </Route>
+
+          <Route path='notes'>
+            <Route index element={<NotesList />} />
+            <Route path=':id' element={<EditNote />} />
+            <Route path='new' element={<NewNote />} />
           </Route>
         </Route>
         {/* End Dash */}
---
+++ tech-notes-react/src/config/roles.js
@@ -0,0 +1,5 @@
+export const ROLES = {
+  Employee: 'Employee',
+  Maneger: 'Maneger',
+  Admin: 'Admin',
+};
